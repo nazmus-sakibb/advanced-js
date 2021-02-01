28185f7aa3a02d9db14435d2f3f0cf67df09730e
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-02-01 23:49:43 +0600

    Null vs Undefined completed for the interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name : "Smart boy", phone : 08989};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 4, 5, 6];
+console.log(ages[12]);
